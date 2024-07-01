9e68cff5c3a8ee3bebe527d6008d9c90f3a7ff0e
Shubham Palde <shubhampalde96@gmail.com>
2024-07-01 12:37:26 +0530

    Print Function Basics

---
+++ print.py
@@ -0,0 +1,8 @@
+print("Shubham vishnu palde", end='-')
+print(1)
+#output:Shubham vishnu palde-1
+
+'''
+ata python madhe output he new line var na yeta tyach line var al because python chya print function madhe kahi inbuilt functions ahe jas ki end='\n' je default ahe ani apan jar tyachi value change keli tar output he new line var n yeta tyach line madhe yet similiarly in case of sep='' jyachi default value hi space ahe ani jar ti change keli tar according to the value output ts print hoil
+'''
+
